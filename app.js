bc39dbab80d90556999116123da8bee7f5d4ee94
amanindianic333 <aman.yadav@indianic.com>
2023-08-30 12:12:15 +0530

    as

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+const PORT=process.env.PORT || 8036;
+
 app.get('/get', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8036, function () {
+var server = app.listen(PORT, function () {
    var host = server.address().address
    var port = server.address().port
    
